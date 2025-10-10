b23f4f72d5cf941405d75d3fcc0441699bdb2c79
Denis <denesic@gmail.com>
2025-10-10 21:29:33 +0300

    FSB-74-quality-module (#57)

--- game_utils.lua
+++ game_utils.lua
@@ -1,7 +1,6 @@
 local game_utils = {}
 
 local algorithm = require("llib.algorithm")
-require("util")
 
 function game_utils.items_key_fn(v)
   return v.value.name .. "|" .. v.value.type .. "|" .. v.value.quality
@@ -38,60 +37,6 @@ function game_utils.merge_duplicates(array, merge_fn, key_fn)
   end
 
   return result
-end
-
-local quality_prototypes = nil
-local quality_index_map = nil
-
-local function init_quality_order()
-  local qualities_proto = {}
-
-  for _, proto in pairs(prototypes.quality) do
-    if not proto.hidden then
-      table.insert(qualities_proto, proto)
-    end
-  end
-
-  table.sort(qualities_proto, function(a, b)
-    return a.order < b.order
-  end)
-
-  local index_map = {}
-  for i, proto in ipairs(qualities_proto) do
-    index_map[proto.name] = i
-  end
-
-  return qualities_proto, index_map
-end
-
-function game_utils.get_all_qualities()
-  if not quality_prototypes then
-    quality_prototypes, index_map = init_quality_order()
-  end
-  return quality_prototypes, index_map
-end
-
-function game_utils.get_quality_index(quality_name)
-  if not quality_index_map then
-    quality_prototypes, quality_index_map = init_quality_order()
-  end
-  local index = quality_index_map[quality_name]
-  assert(index)
-  return index
-end
-
-function game_utils.get_all_better_qualities(quality_name)
-  local qualities, index_map = game_utils.get_all_qualities()
-
-  local index = index_map[quality_name]
-  assert(index)
-
-  local betters = {}
-  for i = index + 1, #qualities do
-    table.insert(betters, qualities[i])
-  end
-
-  return betters
 end
 
 function game_utils.is_fluid(item)
